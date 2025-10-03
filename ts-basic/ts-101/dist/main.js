f61b06b6183488449a4f7bca317dbc03d5d54a2a
Montasir Azad <montasirazad@gmail.com>
2025-10-04 01:35:47 +0600

    ts begins

---
+++ ts-basic/ts-101/dist/main.js
@@ -0,0 +1,8 @@
+console.log(30000000);
+const hello = (name) => {
+    console.log(`hello from ${name}`);
+};
+hello("john");
+hello(11);
+export {};
+//# sourceMappingURL=main.js.map
---
+++ ts-basic/ts-101/src/main.ts
@@ -0,0 +1,7 @@
+console.log(30000000);
+const hello = (name: string | number) => {
+  console.log(`hello from ${name}`);
+};
+
+hello("john");
+hello(11);
